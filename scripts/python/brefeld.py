fda7b97fb7235a5f75c566e77baffab0e77b3e2d
David <david.dk.kube@gmail.com>
2020-10-23 22:06:35 +0200

    deleted old files

--- scripts/python/brefeld.py
+++ scripts/python/brefeld.py
@@ -17,9 +17,9 @@ PITCH_LENGTH = 107
 PITCH_WIDTH = 70
 X_BIN_WIDTH = PITCH_LENGTH/X_SEGMENTATION
 Y_BIN_WIDTH = PITCH_WIDTH/Y_SEGMENTATION
-X_BINS= [-5] + [X_BIN_WIDTH*i for i in range(1,X_SEGMENTATION)] + [PITCH_LENGTH+5] 
-X_LABELS = list(range(0,X_SEGMENTATION))          
-Y_BINS= [-5] + [Y_BIN_WIDTH*i for i in range(1,Y_SEGMENTATION)] + [PITCH_WIDTH + 5] 
+X_BINS= [-5] + [X_BIN_WIDTH*i for i in range(1,X_SEGMENTATION)] + [PITCH_LENGTH+5]
+X_LABELS = list(range(0,X_SEGMENTATION))
+Y_BINS= [-5] + [Y_BIN_WIDTH*i for i in range(1,Y_SEGMENTATION)] + [PITCH_WIDTH + 5]
 Y_LABELS = list(range(0,Y_SEGMENTATION))
 PRE_TIMES = [0.2]
 TIMES = [0.1,0.5,1,2,3,4,5]
@@ -36,9 +36,9 @@ FILE_DICT = {}
 def ifelse(boo, yes, no):
     if (boo):
         return(yes)
-    return(no) 
+    return(no)
 
-#Brefeld's tranformation function. Takes in 3 (x,y) coordinates and outputs a single coordinate 
+#Brefeld's tranformation function. Takes in 3 (x,y) coordinates and outputs a single coordinate
 #referring to the transformed position of the player given origin as starting point
 def transform(ps,pt,pu):
     r = math.sqrt((pt[0]-pu[0])**2+(pt[1]-pu[1])**2)
@@ -50,7 +50,7 @@ def transform(ps,pt,pu):
 def transformRowTriplets(row):
     #print([[float(row.psx),float(row.psy)],[float(row.ptx),float(row.pty)],[float(row.pux),float(row.puy)]])
     return(transform([float(row.psx),float(row.psy)],[float(row.ptx),float(row.pty)],[float(row.pux),float(row.puy)]))
-     
+
 class PPCTime(object):
 
     def __init__(self,f):
@@ -64,7 +64,7 @@ class PPCTime(object):
             for pos in temp.groupby(['speedGroup','angleGroup']):
                 pos=pos[1]
                 sp = int(pos['speedGroup'].values[0])
-                ang = int(pos['angleGroup'].values[0])       
+                ang = int(pos['angleGroup'].values[0])
                 x = [x for x in list(pos['xBlock'].values) if str(x) != 'nan']
                 y = [x for x in list(pos['yBlock'].values) if str(x) != 'nan']
                 xEnd = [x for x in list(pos['xEndBlock'].values) if str(x) != 'nan']
@@ -72,6 +72,8 @@ class PPCTime(object):
                 b = [(a*Y_SEGMENTATION + b)*NUM_CELLS + c*Y_SEGMENTATION + d for a,b,c,d in zip(x,y,xEnd,yEnd)]
                 b  = Counter(b)
                 for k in b.keys():
+                    if (math.floor(int(k)/NUM_CELLS) == 0):
+                        print(k)
                     big_mat[sp-1][ang-1][math.floor(int(k)/NUM_CELLS)][int(k)%NUM_CELLS] += b[k]
 
             with self.lock:
@@ -80,7 +82,7 @@ class PPCTime(object):
         pool.apply_async(do_time,list(range(len(TIMES))))
         pool.close()
         pool.join()
-        
+
 class BrefeldPlayer(object):
 
     def __init__(self,f,given_time,given_player):
@@ -89,7 +91,7 @@ class BrefeldPlayer(object):
         self.player = given_player
 
     def update(self,model):
-        self.file[str(model[0])][self.time] = self.file[str(model[0])][self.time] + model[1] 
+        self.file[str(model[0])][self.time] = self.file[str(model[0])][self.time] + model[1]
 
     def process(self,sp):
         sped = int(sp['speedGroup'].values[0])
@@ -102,27 +104,27 @@ class BrefeldPlayer(object):
             cnt[int(k)] = b[k]
         self.update([sped,cnt])
 
-def write_folders():             
+def write_folders():
     for team in TEAMS:
         if not os.path.exists(PATH_BRE + '/' + team):
-            os.mkdir(PATH_BRE + '/' + team)                 
-            
+            os.mkdir(PATH_BRE + '/' + team)
+
 def extract_triplets_into_matrices(filename,zp) :
     start_time = time.process_time()
     print(filename)
-   
+
     #read in file
     match = pd.read_csv(zp.open(filename))
-    
+
     #remove ball
     match = match[match.player != 1]
-    match = match.iloc[:,1:]    
+    match = match.iloc[:,1:]
     match = match.loc[(match['state'] == ' play_on')]
-    
+
     matchID_name = match.iloc[0,0]
-    
+
     players = pd.Categorical(match['player']).categories
-    
+
     for ind1 in range(0,len(PRE_TIMES)):
         t = PRE_TIMES[ind1]
         preGap = t/0.1
@@ -138,10 +140,10 @@ def extract_triplets_into_matrices(filename,zp) :
                                                play_df.iloc[0,6])
             team_side = ifelse(int(player_id) < 13,
                                                "l",
-                                               "r")                    
+                                               "r")
             print(team_name + " " + str(player_name))
-            
-            def form_temp(df): 
+
+            def form_temp(df):
                 main = pd.DataFrame(columns=['matchID','team','side','player',
                                              'psx','psy',
                                              'ptx','pty',
@@ -150,11 +152,11 @@ def extract_triplets_into_matrices(filename,zp) :
                                              'angle','angleGroup',
                                              'td','tD',
                                              'xBlock','yBlock',
-                                             'xEndBlock','yEndBlock'])                
+                                             'xEndBlock','yEndBlock'])
                 for ind2 in range(0,len(TIMES)):
-                    
+
                     T = TIMES[ind2]
-                    gap = T/0.1                
+                    gap = T/0.1
                     psx = df.iloc[0:int(rows-preGap-gap),:].x
                     psy = df.iloc[0:int(rows-preGap-gap),:].y
                     ptx = df.iloc[int(preGap):int(rows-gap),:].x
@@ -164,8 +166,8 @@ def extract_triplets_into_matrices(filename,zp) :
                     speed = df.iloc[int(preGap):int(rows-gap),:].speed
                     speedGroup = df.iloc[int(preGap):int(rows-gap),:].speedGroup
                     angle = df.iloc[int(preGap):int(rows-gap),:].angle
-                    angleGroup = df.iloc[int(preGap):int(rows-gap),:].angleGroup   
-                    
+                    angleGroup = df.iloc[int(preGap):int(rows-gap),:].angleGroup
+
                     #create id columns
                     x_len = len(psx)
                     td = [t]*x_len
@@ -174,15 +176,15 @@ def extract_triplets_into_matrices(filename,zp) :
                     player = [player_name]*x_len
                     team = [team_name]*x_len
                     side = [team_side]*x_len
-                    
+
                     if (team_side == "r"):
                         psx = PITCH_LENGTH - psx
                         ptx = PITCH_LENGTH - ptx
                         pux = PITCH_LENGTH - pux
                         psy = PITCH_WIDTH - psy
                         pty = PITCH_WIDTH - pty
-                        puy = PITCH_WIDTH - puy                        
-                    
+                        puy = PITCH_WIDTH - puy
+
                     #append temporary data frame
                     temp = pd.DataFrame(list(zip(matchID,team,side,player,
                                                 psx,psy,ptx,pty,
@@ -195,15 +197,15 @@ def extract_triplets_into_matrices(filename,zp) :
                                                  'speed','speedGroup',
                                                  'angle','angleGroup',
                                                  'td','tD'])
-                    
-                    temp['xBlock'] = pd.cut(temp['ptx'], bins=X_BINS, labels=X_LABELS,include_lowest = True)  
-                    temp['yBlock'] = pd.cut(temp['pty'], bins=Y_BINS, labels=Y_LABELS,include_lowest = True)                
-                    temp['xEndBlock'] = pd.cut(temp['pux'], bins=X_BINS, labels=X_LABELS,include_lowest = True)  
-                    temp['yEndBlock'] = pd.cut(temp['puy'], bins=Y_BINS, labels=Y_LABELS,include_lowest = True)         
-                    
+
+                    temp['xBlock'] = pd.cut(temp['ptx'], bins=X_BINS, labels=X_LABELS,include_lowest = True)
+                    temp['yBlock'] = pd.cut(temp['pty'], bins=Y_BINS, labels=Y_LABELS,include_lowest = True)
+                    temp['xEndBlock'] = pd.cut(temp['pux'], bins=X_BINS, labels=X_LABELS,include_lowest = True)
+                    temp['yEndBlock'] = pd.cut(temp['puy'], bins=Y_BINS, labels=Y_LABELS,include_lowest = True)
+
                     main = pd.concat([main,temp])
-                return(main)                
-            
+                return(main)
+
             list_of_df = [d for _, d in play_df.groupby(play_df.frame - list(range(rows)))]
             temp = pd.DataFrame(columns=['matchID','team','side','player',
                                              'psx','psy',
@@ -213,15 +215,15 @@ def extract_triplets_into_matrices(filename,zp) :
                                              'angle','angleGroup',
                                              'td','tD',
                                              'xBlock','yBlock',
-                                             'xEndBlock','yEndBlock'])  
+                                             'xEndBlock','yEndBlock'])
             for df in list_of_df:
                 df_temp  = form_temp(df)
                 temp = pd.concat([temp,df_temp])
             if (not temp.empty):
                 f = FILE_DICT["time"]
                 time_writer = PPCTime(f)
-                time_writer.process(temp)  
-                
+                time_writer.process(temp)
+
                 for ind3 in range(len(TIMES)):
                     if (len(temp.loc[temp['tD'] == TIMES[ind3]])>0):
                         St = pd.DataFrame(temp.loc[temp['tD'] == TIMES[ind3]].apply(transformRowTriplets,axis=1).tolist())
@@ -231,17 +233,17 @@ def extract_triplets_into_matrices(filename,zp) :
                         St['xBlock'] = temp.loc[temp['tD'] == TIMES[ind3]]['xBlock'].tolist()
                         St['yBlock'] = temp.loc[temp['tD'] == TIMES[ind3]]['xBlock'].tolist()
                         St['speedGroup'] = temp.loc[temp['tD'] == TIMES[ind3]]['speedGroup'].tolist()
-                        St.dropna()                               
+                        St.dropna()
                         fb = FILE_DICT[team_name + "/player_" + str(player_name)]
                         player_writer = BrefeldPlayer(fb,ind3,player_name)
                         for frame in  [d for _, d in St.groupby(['speedGroup'])]:
                             player_writer.process(frame)
-            print("\t" + str(time.process_time() - player_time))            
+            print("\t" + str(time.process_time() - player_time))
     print("Time " + str(ind1) + ": " + str(time.process_time() - start_time))
-                                
+
 def make_matrices(path = "C:/Users/David/OneDrive/Documents/Work/Thesis/Code/Data/",wf=True):
     if (wf):
-        write_folders()    
+        write_folders()
     FILE_DICT['time'] = h5py.File(PATH_PPC + "/time.hdf5","a",libver='latest')
     if (len(list(FILE_DICT['time'].keys())) == 0):
         for ind in range(len(TIMES)):
@@ -251,8 +253,8 @@ def make_matrices(path = "C:/Users/David/OneDrive/Documents/Work/Thesis/Code/Dat
             FILE_DICT[team + '/player_' + str(i)] = h5py.File(PATH_BRE + "/" + team + "/player_" + str(i) + ".hdf5","a",libver='latest')
             if (len(list(FILE_DICT[team + '/player_' + str(i)].keys())) == 0):
                 for j in range(1,13):
-                    FILE_DICT[team + '/player_' + str(i)].create_dataset(str(j), (len(TIMES),NUM_CELLS), dtype='i2') 
-        
+                    FILE_DICT[team + '/player_' + str(i)].create_dataset(str(j), (len(TIMES),NUM_CELLS), dtype='i2')
+
     with zipfile.ZipFile(path + "matches_formatted.zip") as z:
         for file in list(filter(lambda x:x.endswith(".csv"),z.namelist() )):
             extract_triplets_into_matrices(file,z)
