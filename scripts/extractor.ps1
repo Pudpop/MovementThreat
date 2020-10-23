85ac423a5a7a4c5c4a36193a87bd7e7813a8ad95
David <david.dk.kube@gmail.com>
2020-10-23 22:16:05 +0200

    added ps scripts

---
+++ scripts/extractor.ps1
@@ -0,0 +1,48 @@
+#requires -Version 5.0
+
+# change $Path to a ZIP file that exists on your system!
+$Path = "C:\Users\David\OneDrive\Documents\Work\Thesis\Code\Data"
+
+# change extension filter to a file extension that exists
+# inside your ZIP file
+$Filter = '*moving.csv'
+
+$FilterTwo = '*.zip'
+
+# change output path to a folder where you want the extracted
+# files to appear
+$OutPath = 'C:\Users\David\OneDrive\Documents\Work\Thesis\Code\temp'
+
+# ensure the output folder exists
+$exists = Test-Path -Path $OutPath
+if ($exists -eq $false)
+{
+  $null = New-Item -Path $OutPath -ItemType Directory -Force
+}
+
+# load ZIP methods
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+Get-ChildItem -Path $Path -Filter $FilterTwo |
+    
+    ForEach-Object{
+        # open ZIP archive for reading
+        $zip = [System.IO.Compression.ZipFile]::OpenRead($_.FullName)
+
+        # find all files in ZIP that match the filter (i.e. file extension)
+        $zip.Entries | 
+          Where-Object { $_.FullName -like $Filter } |
+          ForEach-Object { 
+            # extract the selected items from the ZIP archive
+            # and copy them to the out folder
+            $FileName = $_.Name
+            [System.IO.Compression.ZipFileExtensions]::ExtractToFile($_, "$OutPath\$FileName", $true)
+            }
+
+        # close ZIP file
+        $zip.Dispose()
+
+    }
+    
+# open out folder
+explorer $OutPath 
